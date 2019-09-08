87ce3dea2a55bc0d4e9dd1f648d251700b426dcc
kijhtybbu <dgezhao@gmail.com>
2019-09-08 22:11:09 +0800

    调整了admin显示

--- store_manage/admin.py
+++ store_manage/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import StoreInformation
+
+
+class StoreInformationAdmin(admin.ModelAdmin):
+    fields = (('brand', 'code'),
+              ('name', 'address'),
+              'contacts',
+              ('equipment_arrival_date', 'completion_date'),
+              ('expected_installation_date', 'Opening_date'),
+              'engineering_head',
+              'ip',
+              'firewall_type',
+              'ap',
+              'status',
+              'ps')
+    list_display = ('code', 'name', 'ip', 'engineering_head', 'status', 'Opening_date', 'equipment_arrival_date',)
+    date_hierarchy = 'Opening_date'
+    empty_value_display = '(None)'
+    # ordering设置默认排序字段，负号表示降序排序
+    ordering = ('-Opening_date',)
+
+    # list_editable 设置默认可编辑字段
+    # list_editable = ['machine_room_id', 'temperature']
+    list_filter = ('status',)  # 过滤器
+    search_fields = ('code', 'name')  # 搜索字段
+
+
+admin.site.site_header = '店铺管理'
+admin.site.site_title = '店铺管理系统'
+admin.site.register(StoreInformation, StoreInformationAdmin)
+
+
--- storemanage/settings.py
+++ storemanage/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'storemanage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store_manage',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '10.0.21.108',
+        'PORT': '3306',
+        'OPTIONS': {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
@@ -105,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
