c11d62e1615f1723866898e04d000254870de8ff
kijhtybbu <dgezhao@gmail.com>
2020-01-02 15:13:08 +0800

    加入评论

--- store_manage/views.py
+++ store_manage/views.py
@@ -1,11 +1,15 @@
 from django.db.models import Q
+from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, FormView
+from django.views.generic.detail import SingleObjectMixin
 
 from store_manage.forms import CommentForm
-from store_manage.models import StoreInformation
+from store_manage.models import StoreInformation, StoreComment
 
 
 class StoreListView(ListView):
@@ -13,7 +17,37 @@ class StoreListView(ListView):
     queryset = StoreInformation.objects.order_by('-Opening_date')
 
 
-class StoreDetailView(DetailView):
+class StoreDetailView(View):
+    def get(self, request, *args, **kwargs):
+        view = StoreDetailDisplay.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = StoreDetail.as_view()
+        return view(request, *args, **kwargs)
+
+
+class StoreDetail(FormView):
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        sc = StoreComment()
+        sc.content = form.cleaned_data['content']
+        # sc.stor.store = form.cleaned_data['content']
+        sc.store = StoreInformation.objects.get(code=self.kwargs.get('slug'))
+        sc.save()
+        # form.store_id = self.kwargs.get('slug')
+        # form.save()
+        return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('detail', kwargs={'slug': self.kwargs.get('slug')})
+
+
+class StoreDetailDisplay(DetailView):
     model = StoreInformation
     template_name = 'store_manager/detail.html'
     slug_field = 'code'
