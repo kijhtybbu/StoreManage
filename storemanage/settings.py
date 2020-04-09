4072d01c851fa7426d1547f1cde9e1aa0cd0464c
kijhtybbu <dgezhao@gmail.com>
2020-04-09 18:08:28 +0800

    添加 ALLOWED_HOSTS = ['*']

--- storemanage/settings.py
+++ storemanage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7ri^hakp8wp07v+d&f+de&7x-lg2g*vgq&hb-xj)k)ga4es$lo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
